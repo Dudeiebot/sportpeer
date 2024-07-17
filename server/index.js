9e7b81283a19300e6767ae7868dc6ef1896e1743
Dudeiebot <ojoexcel5@gmail.com>
2024-07-17 11:07:12 +0100

    index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+
+// Use cookie-parser middleware before any middleware that relies on cookies
+app.use(cookieParser());
+
+// Debugging middleware
+app.use((req, res, next) => {
+  console.log("Cookies:", req.cookies);
+  next();
+});
+
+// CORS configuration
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
